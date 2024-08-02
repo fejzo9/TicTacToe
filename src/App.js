cc29af9a470ca121cc1da5790faf02504ca0780f
fejzo9 <fejzo999@gmail.com>
2024-08-02 20:10:08 +0200

    Added a function calculateWinner which checks has already someone from players won, and declare a winner.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
  
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -40,4 +40,24 @@ export default function Board() {
 
 function Square({value, onSquareClick}){
   return (<button className="square" onClick={onSquareClick}>{value}</button>);
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
